eac6adcadbfc6c40bb93fb5058fb3b377f613075
StraponBace <ponomarev.tima2005@mail.ru>
2024-02-29 12:37:05 +0700

    first column

--- js/main.js
+++ js/main.js
@@ -2,23 +2,28 @@ Vue.component('kanban',{
     template:`
         <div class='app'> 
             <h1>KABAN TABLE</h1>
-            <create-task-form></create-task-form>
-            <div class="colums">
-                <column></column>
-                <column></column>
-                <column></column>
-                <column></column>
+            <create-task-form @task-created="createTask"></create-task-form>
+            <div class="columns">
+                <column :tasks="plannedTasks"></column>
+<!--                <column></column>-->
+<!--                <column></column>-->
+<!--                <column></column>-->
             </div>
         </div>
     `,
     data(){
         return{
-
+            plannedTasks: []
+        }
+    },
+    methods:{
+        createTask(task) {
+            this.plannedTasks.push(task);
         }
     }
 })
 
-Vue.component('create-tsk-form',{
+Vue.component('create-task-form',{
    template: `
         <div class="task-form">
             <h2>Create task</h2>
@@ -27,7 +32,43 @@ Vue.component('create-tsk-form',{
             <input type="date" v-model="deadline">
             <button @click="createTask">Create</button>
         </div>
-   `
+   `,
+    methods:{
+        createTask() {
+            const newTask = {
+                title: this.title,
+                description: this.description,
+                deadline: this.deadline,
+                lastEdited: new Date(),
+                column: 'plannedTasks'
+            };
+
+            this.$emit('task-created', newTask);
+
+            this.title = '';
+            this.description = '';
+            this.deadline = '';
+        }
+    }
+})
+
+Vue.component('column',{
+    props:['tasks', 'task'],
+    template:`
+        <div>
+            <div class="column">
+                <div class="tasks" >
+                    <div class="task-card" v-for="task in tasks" :key="task.id" :task="task">
+                        <h3>{{ task.title }}</h3>
+                        <p>{{ task.description }}</p>
+                        <p>Deadline: {{ task.deadline }}</p>
+                        <p>Last change: {{ task.lastEdited }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `,
+
 })
 
 let app = new Vue({
