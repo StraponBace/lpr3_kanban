609dc8f9437e50f19d154a1ad07431619b9b67d2
StraponBace <ponomarev.tima2005@mail.ru>
2024-02-29 11:58:10 +0700

    template create task form

--- js/main.js
+++ js/main.js
@@ -1,5 +1,33 @@
 Vue.component('kanban',{
+    template:`
+        <div class='app'> 
+            <h1>KABAN TABLE</h1>
+            <create-task-form></create-task-form>
+            <div class="colums">
+                <column></column>
+                <column></column>
+                <column></column>
+                <column></column>
+            </div>
+        </div>
+    `,
+    data(){
+        return{
 
+        }
+    }
+})
+
+Vue.component('create-tsk-form',{
+   template: `
+        <div class="task-form">
+            <h2>Create task</h2>
+            <input type="text" v-model="title" placeholder="Title">
+            <textarea v-model="description" placeholder="Description"></textarea>
+            <input type="date" v-model="deadline">
+            <button @click="createTask">Create</button>
+        </div>
+   `
 })
 
 let app = new Vue({
