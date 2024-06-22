fdb95c5feb4e1a1b237d79b8a34b35f1e3f07e90
StraponBace <ponomarev.tima2005@mail.ru>
2024-06-22 23:35:15 +0700

    error move column

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@ Vue.component('kanban', {
             <h1>KABAN TABLE</h1>
             <create-task-form @task-created="createTask"></create-task-form>
             <div class="columns">
-                <column title="Planned tasks" :tasks="plannedTasks" @task-moved="moveTask" @task-delete="deleteTask"></column>
-                <column title="Tasks in progress" :tasks='inProgressTasks' @task-moved="moveTask"></column>
-                <column title="Task in testing" :tasks="testingTasks" @task-moved="moveTask"></column>
-                <column title="Completed Task" :tasks="completedTasks" @task-returned="returnTask"></column>
+                <column title="Запланированные задачи" :tasks="plannedTasks" @task-moved="moveTask" @task-delete="deleteTask"></column>
+                <column title="Задачи в работе" :tasks='inProgressTasks' @task-moved="moveTask"></column>
+                <column title="Тестирование" :tasks="testingTasks" @task-moved="moveTask" @task-returned="returnTask"></column>
+                <column title="Выполненные задачи" :tasks="completedTasks"></column>
             </div>
         </div>
     `,
@@ -27,9 +27,9 @@ Vue.component('kanban', {
             this[task.column].splice(this[task.column].indexOf(task), 1);
             this[targetColumn].push({...task, column: targetColumn, lastEdited: new Date()});
         },
-        returnTask({task, targetColumn}){
+        returnTask({task, targetColumn, returnReason}){
             this[task.column].splice(this[task.column].indexOf(task), 1);
-            this[targetColumn].push({...task, column: targetColumn, lastEdited: new Date()});
+            this[targetColumn].push({...task, column: targetColumn, lastEdited: new Date(), returnReason: returnReason});
         },
         deleteTask(task){
             const index = this[task.column].findIndex(t => t === task);
@@ -44,11 +44,13 @@ Vue.component('kanban', {
 Vue.component('create-task-form', {
     template: `
         <div class="task-form">
-            <h2>Create task</h2>
-            <input type="text" v-model="title" placeholder="Title">
-            <textarea v-model="description" placeholder="Description"></textarea>
+            <h2>Создание задачи</h2>
+            <input type="text" v-model="title" placeholder="Заголовок">
+            <textarea v-model="description" placeholder="Описание"></textarea>
+            <label for="deadline">Дедлайн:</label>
             <input type="date" v-model="deadline">
-            <button @click="createTask">Create</button>
+            <h4 v-if="title == '' || description == '' || deadline ==''">Для создания задачи заполните все поля</h4>
+            <button @click="createTask" v-if="title !== '' && description !== '' && deadline !==''">Создать</button>
         </div>
    `,
     data() {
@@ -93,8 +95,8 @@ Vue.component('column', {
         moveTask(newColumn) {
             this.$emit('task-moved', {task: newColumn.task, targetColumn: newColumn.targetColumn})
         },
-        returnTask(newColumn){
-            this.$emit('task-returned', {task: newColumn.task, targetColumn: newColumn.targetColumn})
+        returnTask(newColumn) {
+            this.$emit('task-returned', {task: newColumn.task, targetColumn: newColumn.targetColumn, returnReason: newColumn.returnReason});
         },
         deleteTask(task){
             this.$emit('task-delete', task);
@@ -111,21 +113,29 @@ Vue.component('task-card', {
             <input v-else type="text" v-model="newTitle" />
             <p v-if="!isEditing">{{ task.description }}</p>
             <textarea v-else v-model="newDescription" />
-            <p>Deadline: {{ task.deadline }}</p>
-            <p v-if="allowStatus">Status : {{ checkDeadline() }}</p>
-            <p>Last change: {{ task.lastEdited }}</p>
-            <button v-if="allowMove" @click="moveToNextColumn">Move to next column</button>
-            <button v-if="allowReturn" @click="returnTaskToFirstColumn">Return task</button>
-            <button v-if="allowEdit && !isEditing" @click="startEditing">Edit task</button>
-            <button v-if="isEditing" @click="saveEdit">Save edit</button>
-            <button v-if="allowDel" @click="deleteTask">Delete task</button>
+            <input v-if="isReturning" type="text" v-model="returnReason" placeholder="Причина возврата" />
+            <p>Дедлайн: {{ task.deadline }}</p>
+            <p>Последнее изменение: {{ task.lastEdited }}</p>
+            <p v-if="task.returnReason">Причина возврата: {{ task.returnReason }}</p>
+            <p v-if="allowStatus">Статус : {{ checkDeadline() }}</p>
+            <div class="buttons">
+                <button v-if="isReturning" @click="saveReturnReason">Сохранить причину возврата</button>
+                <button v-if="allowMove && !isEditing && !isReturning" @click="moveToNextColumn">Переместить дальше</button>
+                <button v-if="allowReturn && !isEditing && !isReturning" @click="returnTaskToSecondColumn">Вернуть задачу</button>
+                <button v-if="allowEdit && !isEditing && !isReturning" @click="startEditing">Редактировать задачу</button>
+                <button v-if="isEditing && !isReturning" @click="saveEdit">Сохранить изменения</button>
+                <button v-if="allowDel && !isEditing && !isReturning" @click="deleteTask">Удалить задачу</button>
+            </div>
         </div>
     `,
     data() {
         return {
             isEditing: false,
             newTitle: '',
-            newDescription: ''
+            newDescription: '',
+            isReturning: false,
+            returnReason: '',
+            returnReasonDisplay: ''
         }
     },
     methods: {
@@ -143,8 +153,13 @@ Vue.component('task-card', {
             this.task.lastEdited = new Date();
             this.isEditing = false;
         },
-        returnTaskToFirstColumn(){
-            this.$emit('return', {task: this.task, targetColumn: returnInFirstColumn(this.task.column)})
+        returnTaskToSecondColumn() {
+            this.isReturning = true;
+        },
+        saveReturnReason() {
+            this.$emit('return', {task: this.task, targetColumn: returnInSecondColumn(this.task.column), returnReason: this.returnReason});
+            this.task.returnReason = this.returnReason;
+            this.isReturning = false;
         },
         deleteTask(){
             this.$emit('delete', this.task)
@@ -153,21 +168,37 @@ Vue.component('task-card', {
             const deadline = new Date(this.task.deadline);
             const now = new Date();
             if(now > deadline){
-                return this.task.status = 'overdue';
+                return this.task.status = 'Просрочено';
             }else{
-                return this.task.status = 'completed';
+                return this.task.status = 'Выполнено в срок';
+            }
+        },
+        getNextColumn(currentColumn) {
+            switch (currentColumn) {
+                case 'plannedTasks':
+                    return 'inProgressTasks';
+                case 'inProgressTasks':
+                    return 'testingTasks';
+                case 'testingTasks':
+                    return 'completedTasks';
+            }
+        },
+        returnInSecondColumn(currentColumn){
+            switch (currentColumn){
+                case 'testingTasks':
+                    return 'inProgressTasks';
             }
         }
     },
     computed:{
         allowEdit(){
-            return this.task.column === 'testingTasks';
+            return this.task.column === 'plannedTasks' || this.task.column === 'inProgressTasks' || this.task.column === 'testingTasks';
         },
         allowMove(){
             return this.task.column === 'plannedTasks' || this.task.column === 'inProgressTasks' || this.task.column === 'testingTasks';
         },
         allowReturn(){
-            return this.task.column === 'completedTasks';
+            return this.task.column === 'testingTasks';
         },
         allowDel(){
             return this.task.column === 'plannedTasks'
@@ -175,32 +206,8 @@ Vue.component('task-card', {
         allowStatus(){
             return this.task.column === 'completedTasks';
         }
-    },
-    mounted(){
-        if(this.task.column === 'completedTasks'){
-            this.checkDeadline();
-        }
     }
 })
-
-function getNextColumn(currentColumn) {
-    switch (currentColumn) {
-        case 'plannedTasks':
-            return 'inProgressTasks';
-        case 'inProgressTasks':
-            return 'testingTasks';
-        case 'testingTasks':
-            return 'completedTasks';
-    }
-}
-
-function returnInFirstColumn(currentColumn){
-    switch (currentColumn){
-        case 'completedTasks':
-            return 'plannedTasks';
-    }
-}
-
 
 let app = new Vue({
     el: '#app'
